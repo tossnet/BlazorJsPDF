71c2c3ece29e90e8690b17902a2369a3f7f7d962
Christophe Peugnet <christophe.peugnet@outlook.fr>
2023-01-24 15:10:34 +0100

    .

---
+++ Pages/Index.razor.js
@@ -0,0 +1,26 @@
+﻿export function generatePdf(idelement, filename) {
+
+    var el = document.getElementById(idelement);
+    //
+    const pdf = new jspdf.jsPDF({
+        orientation: 'landscape',
+        unit: 'pt',
+        format: 'a4',
+        hotfixes: ["px_scaling"]
+    });
+
+    return new Promise((resolve, reject) => {
+        pdf.html(el, {
+            html2canvas: {
+                scale: 0.6
+            },
+            callback: pdf => {
+                pdf.save(filename);
+                resolve();
+            },
+            margin: [10, 10, 10, 10],
+            x: 0,
+            y: 30,
+        });
+    });
+}
